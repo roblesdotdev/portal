3a81b695eb866bfd4e1fdf5b4652ef1762d550c7
roblesdotdev <httpsnip@gmail.com>
2024-09-26 14:08:15 -0300

    setup format rules with prettier

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,16 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
+  ]
+}
 
 export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
       <h1>Working</h1>
     </div>
-  );
+  )
 }
